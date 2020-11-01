fcf8ee806c683d5a72193d9279b620946b1aeacc
chucklapress <charleslapress@gmail.com>
2020-11-01 17:06:26 -0500

    modified signature script to commit

--- level2.py
+++ level2.py
@@ -1,12 +1,25 @@
 from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+import binascii
 
-key = RSA.generate(2048)
-private_key = key.export_key()
-file_out = open("./keys/private.pem", "wb")
-file_out.write(private_key)
-file_out.close()
+keyPair = RSA.generate(3072)
 
-public_key = key.publickey().export_key()
-file_out = open("./keys/receiver.pem", "wb")
-file_out.write(public_key)
-file_out.close()
+pubKey = keyPair.publickey()
+print(f"Public key:  (n={hex(pubKey.n)}, e={hex(pubKey.e)})")
+pubKeyPEM = pubKey.exportKey()
+print(pubKeyPEM.decode('ascii'))
+
+print(f"Private key: (n={hex(pubKey.n)}, d={hex(keyPair.d)})")
+privKeyPEM = keyPair.exportKey()
+print(privKeyPEM.decode('ascii'))
+
+
+msg = b'SECRET ENCRYPTED MESSAGE HELLO CHUCK'
+encryptor = PKCS1_OAEP.new(pubKey)
+encrypted = encryptor.encrypt(msg)
+print("Encrypted:", binascii.hexlify(encrypted))
+
+
+decryptor = PKCS1_OAEP.new(keyPair)
+decrypted = decryptor.decrypt(encrypted)
+print('Decrypted:', decrypted)
